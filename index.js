3a9744d3eafcdd12475290294b105cb3b48a0fc2
emilianostic <emilianostic@gmail.com>
2024-08-28 16:05:18 -0300

    Hasta la seccion HTTP Response

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 //HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, and DELETE. These correspond to create, read, update, and delete (or CRUD)
-const express = require('express')
+const express = require("express");
 //thunderClient: Client REST para no tener que crear frontend y poder hacer las peticiones
 var app = express();
 
-app.get('/products', (req, res)=>{
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-    //query a database (consultar DB)
-    //process data
-    //validate data
+app.get("/isAlive", (req, res) => {
+    res.sendStatus(204);//no va a mostrar nada el navegador, si en herram de desarroll
+  });
+  
 
+app.get("/miarchivo", (req, res) => {
+  res.sendFile("./javascript.png", {
+    // sin express tendría que haber importado 'fs'
+    root: __dirname,
+  });
+});
 
-    res.send('lista de productos')
-})
-app.post('/products', (req, res)=>{
-    res.send('creando productos')
-})
+app.get("/user", (req, res) => {
+  res.json({
+    name: "Emi",
+    lastname: "Vela",
+    Age: 43,
+    points: [20, 30, 40],
+    address: {
+      city: "Parana",
+      street: "Las Lengas 666",
+    },
+  });
+});
 
-app.put('/products', (req, res)=>{
-    res.send('actualizando un producto')
-})
-
-app.delete('/products', (req, res)=>{
-    res.send('eliminando un producto')
-})
-
-app.patch('/products', (req, res)=>{
-    res.send('actualizando una parte del producto')
-})
-
-
-app.listen(3000)
-console.log(`Serve on port ${3000}`) 
+app.listen(3000);
+console.log(`Serve on port ${3000}`);
