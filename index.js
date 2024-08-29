982be6d2e0cd93d7ad2d48152a5a7581dd6ae877
emilianostic <emilianostic@gmail.com>
2024-08-29 15:56:37 -0300

    Hasta la seccion Request Body

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-//HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, and DELETE. These correspond to create, read, update, and delete (or CRUD)
+
 const express = require("express");
-//thunderClient: Client REST para no tener que crear frontend y poder hacer las peticiones
-var app = express();
+//el request no es solo un título. Es un doc: título (url por ej), contenido(json por ejem), cabeceras (info del contenido). Partes: /about 'endpoint', {img, text, etc } "body" y "header" nota adicional que describ y va el código de estado. Es importante en express
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+const app = express();
+app.use(express.urlencoded({extended: false})) //para formularios
+app.use(express.json())
+//app.use(express.text()) con esto puede procesar el texto
+app.post('/user', (req, res)=>{
+    console.log(req.body)
+    res.send('usario creado')
+})
 
-app.get("/isAlive", (req, res) => {
-    res.sendStatus(204);//no va a mostrar nada el navegador, si en herram de desarroll
-  });
-  
-
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./javascript.png", {
-    // sin express tendría que haber importado 'fs'
-    root: __dirname,
-  });
-});
-
-app.get("/user", (req, res) => {
-  res.json({
-    name: "Emi",
-    lastname: "Vela",
-    Age: 43,
-    points: [20, 30, 40],
-    address: {
-      city: "Parana",
-      street: "Las Lengas 666",
-    },
-  });
-});
 
 app.listen(3000);
 console.log(`Serve on port ${3000}`);
