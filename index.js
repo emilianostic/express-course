b79764505f2d800cadf342ba80583522b8301810
emilianostic <emilianostic@gmail.com>
2024-08-30 15:15:51 -0300

    Hasta la seccion midleware

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
-
 const express = require("express");
-const app = express()
+const app = express();
+//middleware
+app.use((req, res, next) => {
+    console.log(`Route: ${req.url} Metodo: ${req.method}`);
+    next();
+  });
 
-//all method funciona como PUT GET POST DELETE
-app.all('/info', (req, res)=>{
- 
-    res.send('server info')
-})
+app.get("/profile", (req, res) => {
+    res.send("profile page");
+  });
 
-app.get('/search', (req, res)=>{
-    console.log(req.query)
-    if(req.query.q === 'javascript books'){//en la url coloco search?q=.... % es el espacio
-res.send('lista de libros J')
-    }
-    else{
-        res.send('pagina normal')
-    }
-    })
-app.get('/hello/:username', (req, res)=>{
-console.log(req.query)//esto se ve si coloco "?" en la url en el navegador. Se ve como un objeto
+//middleware
+app.use((req, res, next) => {
+  if (req.query.login === "emilianostic.com") {
+    next();
+  } else {
+    res.send("no autorizado");
+  }
+});
 
-console.log(req.query.user)
-console.log(req.query.age)
-    res.send(`Hello ${req.params.username.toUpperCase()}`)
-})
-
-app.get('/add/:x/:y', (req, res)=>{
-    const {x, y} = req.params
-const result = parseInt(x) + parseInt(y)
-    res.send(`Result ${result}`)
-})
-
-app.get('/users/:name/photo', (req, res)=>{//parametro desde la url
-   if(req.params.name === "emi"){
-    return res.sendFile('./Javascript.png', {
-        root: __dirname
-    })
-   }
-   res.send('usuario sin acceso')
-    
-})
+app.get("/dashboard", (req, res) => {
+  res.send("dashboard page");
+});
 
 
+
+app.all("/about", (req, res) => {
+  res.send("about page");
+});
 
 app.listen(3000);
 console.log(`Serve on port ${3000}`);
