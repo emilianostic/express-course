a6aa092b55daec5a2b829a02b84e6d5644cf6853
emilianostic <emilianostic@gmail.com>
2024-08-30 13:44:57 -0300

    Hasta la seccion Query

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
 const express = require("express");
 const app = express()
-app.get('/hello/:username', (req, res)=>{//parametro desde la url
 
+app.get('/search', (req, res)=>{
+    console.log(req.query)
+    if(req.query.q === 'javascript books'){//en la url coloco search?q=.... % es el espacio
+res.send('lista de libros J')
+    }
+    else{
+        res.send('pagina normal')
+    }
+    })
+app.get('/hello/:username', (req, res)=>{
+console.log(req.query)//esto se ve si coloco "?" en la url en el navegador. Se ve como un objeto
+
+console.log(req.query.user)
+console.log(req.query.age)
     res.send(`Hello ${req.params.username.toUpperCase()}`)
 })
 
-app.get('/add/:x/:y', (req, res)=>{//parametro desde la url
+app.get('/add/:x/:y', (req, res)=>{
     const {x, y} = req.params
 const result = parseInt(x) + parseInt(y)
     res.send(`Result ${result}`)
