564f32324898b77267926f2c152ac22c017f1ccd
emilianostic <emilianostic@gmail.com>
2024-08-31 19:03:48 -0300

    hasta settings

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const express = require("express");
 const morgan = require('morgan')
 
 const app = express();
+
+app.set('case sensitive routing', true) //para Case sensitive
+app.set('appName', 'Express Course')
+app.set('port', 3000)
+
+
 //middleware, MORGAN
 app.use(morgan('dev'));//muestra mensaje por consola
 
+
 app.get("/profile", (req, res) => {
-    res.send("profile page");
-  });
+  res.send("profile page");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 
-app.get("/dashboard", (req, res) => {
+app.get("/Dashboard", (req, res) => {
   res.send("dashboard page");
 });
 
@@ -21,5 +32,5 @@ app.all("/about", (req, res) => {
   res.send("about page");
 });
 
-app.listen(3000);
-console.log(`Serve on port ${3000}`);
+app.listen(`${app.get('port')}`);
+console.log(`Serve ${app.get('appName')} on port ${app.get('port')}`);
