a1d054013141dc3763b0a8f2585caafe2153baf5
emilianostic <emilianostic@gmail.com>
2024-09-01 14:27:48 -0300

    hasta carpeta static

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const morgan = require('morgan')
 
 const app = express();
+const path = require('path')
 
+//settings
 app.set('case sensitive routing', true) //para Case sensitive
 app.set('appName', 'Express Course')
 app.set('port', 3000)
@@ -12,6 +14,10 @@ app.set('port', 3000)
 //middleware, MORGAN
 app.use(morgan('dev'));//muestra mensaje por consola
 
+
+app.get("/nota.txt", (req, res) => {
+  res.send("esto no es un archivo");
+});
 
 app.get("/profile", (req, res) => {
   res.send("profile page");
@@ -26,11 +32,14 @@ app.get("/Dashboard", (req, res) => {
   res.send("dashboard page");
 });
 
-
+console.log(__dirname)
 
 app.all("/about", (req, res) => {
   res.send("about page");
 });
+
+app.use('/public',express.static(path.join(__dirname, 'public')))//se puede acceder a todo lo que está dentro de la carpeta public. Con el primer public obligo a poner el slash por si un archivo se llama igual que una ruta. Va al final delas rutas. Ej nota.txt
+
 
 app.listen(`${app.get('port')}`);
 console.log(`Serve ${app.get('appName')} on port ${app.get('port')}`);
